0e2e3be603709616ff15ade3d6914b0d1ac0e2b7
romain222 <r.plier@outlook.com>
2018-02-27 15:42:48 +0100

    Completed Exercise 3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,19 @@
 <?php
 
 $input;
-$winner;
+
+$APoints = 0;
+$BPoints = 0;
+
+foreach ($input AS $currentTurn){
+    if($currentTurn[0] > $currentTurn[1]) $APoints++;
+    if($currentTurn[0] < $currentTurn[1]) $BPoints++;
+}
+
+
+if($APoints > $BPoints){
+    $winner = 'A';
+}else{
+    $winner = 'B';
+}
+echo $winner;
