3fc723292b6ca0ae4a6692a6c1e968314b5476b8
romain222 <r.plier@outlook.com>
2018-02-27 12:03:29 +0100

    Completed Exercise 2

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,5 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$saltedPassword = substr($password, 0, floor(strlen($password)/2)) . $salt . substr($password, ceil(strlen($password)/2), strlen($password));
+echo $saltedPassword;
